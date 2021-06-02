5cea17b7b34a6fec9a62916aabb80e1ceb33dd47
AnnaSanna <ewigdieseite@gmail.com>
2021-06-02 20:03:10 +0300

    Изменено название класса, стиль кода

---
+++ sandbox/src/main/java/ru/java_for_testers/sandbox/MyFirstProgram.java
@@ -0,0 +1,8 @@
+package ru.java_for_testers.sandbox;
+
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+    System.out.println("haaaaaaaaaaaay");
+  }
+}
--- sandbox/src/main/java/ru/java_for_testers/sandbox/program.java
+++
@@ -1,8 +0,0 @@
-package ru.java_for_testers.sandbox;
-
-public class program {
-
-	public static void main (String[] args) {
-		System.out.println("haaaaaaaaaaaay");
-	}
-}
