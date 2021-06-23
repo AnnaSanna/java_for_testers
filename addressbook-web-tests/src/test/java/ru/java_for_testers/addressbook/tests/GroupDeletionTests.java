14fd918d58a230bb15e8104d049debf28de5097a
AnnaSanna <ewigdieseite@gmail.com>
2021-06-23 19:30:18 +0300

    Группы: Сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/GroupDeletionTests.java
@@ -11,18 +11,20 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-
-
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
+
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroupOrContact(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups("delete");
- //   app.getGroupHelper().returnToGroupPage();
+
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 
 }
