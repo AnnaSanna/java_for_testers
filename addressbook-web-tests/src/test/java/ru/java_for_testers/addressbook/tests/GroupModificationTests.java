15180c00684a371c1d59712408d72888d48d23a0
AnnaSanna <ewigdieseite@gmail.com>
2021-06-23 19:51:32 +0300

    Группы: Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.java_for_testers.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -17,10 +18,16 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroupOrContact(before.size() -1);
     app.getGroupHelper().initGroupModification("edit");
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification("update");
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() -1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+
   }
 }
