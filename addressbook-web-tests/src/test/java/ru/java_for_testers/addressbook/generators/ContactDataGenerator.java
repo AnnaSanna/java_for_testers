1044164f1e515c6c71ee54e1b1380d328f90df4b
AnnaSanna <ewigdieseite@gmail.com>
2021-07-05 17:58:50 +0300

    генератор для контактов

---
+++ addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,39 @@
+package ru.java_for_testers.addressbook.generators;
+
+import ru.java_for_testers.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  public static void main (String[] args) throws IOException {
+    int count = Integer.parseInt(args[0]);
+    File file = new File(args[1]);
+
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, file);
+  }
+
+  private static void save(List<ContactData> contacts, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s;%s\n", contact.getLastName(), contact.getFirstName()));
+    }
+    writer.close();
+  }
+
+  private static List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withLastName(String.format("test %s", i))
+              .withFirstName(String.format("first name %s", i)));
+    }
+    return contacts;
+  }
+}
