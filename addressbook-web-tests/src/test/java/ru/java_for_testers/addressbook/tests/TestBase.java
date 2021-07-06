15617015e1da138656ae0f5b20230cbd91a71ba7
AnnaSanna <ewigdieseite@gmail.com>
2021-07-06 17:44:02 +0300

    Описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/TestBase.java
@@ -7,7 +7,8 @@ import ru.java_for_testers.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
