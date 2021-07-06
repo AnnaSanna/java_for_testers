dca366d634bdda124a8cb63b700e05c8df02b7b9
AnnaSanna <ewigdieseite@gmail.com>
2021-07-06 14:32:25 +0300

    Группы: Формат XML

--- addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.java_for_testers.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.java_for_testers.addressbook.model.GroupData;
 import ru.java_for_testers.addressbook.model.Groups;
@@ -8,6 +9,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,26 +19,26 @@ public class GroupCreationTests extends TestBase{
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
-    List<Object[]> list = new ArrayList<Object[]>();
-  //  list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-  //  list.add(new Object[] {new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-  //  list.add(new Object[] {new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+  //  List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null) {
-      String[] split = line.split(";");
-      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      xml += line;
+ //     String[] split = line.split(";");
+ //     list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
       line = reader.readLine();
     }
-    return list.iterator();
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validGroups")
   public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
- //   GroupData group = new GroupData()
- //           .withName(name).withHeader(header).withFooter(footer);
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
