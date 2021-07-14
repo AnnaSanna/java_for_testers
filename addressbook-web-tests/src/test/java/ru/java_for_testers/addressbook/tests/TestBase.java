a1499ae7a46324f3fc83cd5e22aa40f041ce49e7
AnnaSanna <ewigdieseite@gmail.com>
2021-07-14 15:37:11 +0300

    Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/java_for_testers/addressbook/tests/TestBase.java
@@ -1,5 +1,7 @@
 package ru.java_for_testers.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +10,17 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.java_for_testers.addressbook.appmanager.ApplicationManager;
+import ru.java_for_testers.addressbook.model.ContactData;
+import ru.java_for_testers.addressbook.model.Contacts;
+import ru.java_for_testers.addressbook.model.GroupData;
+import ru.java_for_testers.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -38,4 +48,28 @@ public class TestBase {
     public void logTestStop(Method m) {
     logger.info("Stop test " + m.getName());
   }
+
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI")) {
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
+  }
+  public void verifyContactListInUI() {
+    if (Boolean.getBoolean("verifyUI")) {
+      Contacts dbContacts = app.db().contacts();
+      Contacts uiContacts = app.contact().all();
+      assertThat(uiContacts, equalTo(dbContacts.stream()
+              .map((g) -> new ContactData().withId(g.getId())
+                      .withLastName(g.getLastName())
+                      .withFirstName(g.getFirstName())
+                      .withAddress(g.getAddress())
+              )
+              .collect(Collectors.toSet())));
+    }
+  }
+
 }
